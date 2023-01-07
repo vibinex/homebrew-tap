6e5c970be1376f86188ec235f6d9f80fd3ca316e
Tapish Rathore <tapish303@gmail.com>
2023-01-07 11:45:06 +0530

    Fix shasum 256

--- Formula/devprofiler.rb
+++ Formula/devprofiler.rb
@@ -2,7 +2,7 @@ class Tle < Formula
   desc "CLI for creating dev profiles from repository contributions"
   homepage "https://github.com/Alokit-Innovations/dev-profiler"
   url "https://github.com/Alokit-Innovations/dev-profiler/releases/download/v0.1.0/devprofiler-0.1.0-apple-darwin.tar.gz"
-  sha256 "d5929329193abc1fe8f56253a6d360185aeb9faf"
+  sha256 "e0993728ffa2d2bf51c6f241c5ecb659e0db9b5a5bc91a5e0f14dd4c7403ec97"
   version "0.1.0"
 
   def install
